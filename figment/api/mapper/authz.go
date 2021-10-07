18b4f76eda450b41f8184e9994c7033a21d84b2b
Jessica Paczuski <jessica@figment.network>
2021-10-07 06:52:00 -0400

    Add feegrant and authz types (#5)
    * Add feegrant and authz types
    
    * Add revoke_response type

---
+++ figment/api/mapper/authz.go
@@ -0,0 +1,114 @@
+package mapper
+
+import (
+	"fmt"
+
+	"github.com/figment-networks/indexing-engine/structs"
+
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	"github.com/gogo/protobuf/proto"
+)
+
+// AuthzGrantToSub transforms authz.MsgGrant sdk messages to SubsetEvent
+func AuthzGrantToSub(msg []byte) (se structs.SubsetEvent, err error) {
+	m := &authz.MsgGrant{}
+	if err := proto.Unmarshal(msg, m); err != nil {
+		return se, fmt.Errorf("Not a grant type: %w", err)
+	}
+
+	return structs.SubsetEvent{
+		Type:   []string{"grant"},
+		Module: "authz",
+		Additional: map[string][]string{
+			"granter":             {m.Granter},
+			"grantee":             {m.Grantee},
+			"grant_expiration":    {m.Grant.Expiration.String()},
+			"grant_authorization": {string(m.Grant.Authorization.Value)},
+		},
+	}, nil
+}
+
+// AuthzExecResponseToSub transforms authz.MsgExecResponse sdk messages to SubsetEvent
+func AuthzExecResponseToSub(msg []byte) (se structs.SubsetEvent, err error) {
+	m := &authz.MsgExecResponse{}
+	if err := proto.Unmarshal(msg, m); err != nil {
+		return se, fmt.Errorf("Not a exec_response type: %w", err)
+	}
+
+	se = structs.SubsetEvent{
+		Type:   []string{"exec_response"},
+		Module: "authz",
+	}
+
+	for _, result := range m.Results {
+		se.Additional["results"] = append(se.Additional["results"], string(result))
+	}
+
+	return se, nil
+}
+
+// AuthzExecToSub transforms authz.MsgExec sdk messages to SubsetEvent
+func AuthzExecToSub(msg []byte) (se structs.SubsetEvent, err error) {
+	m := &authz.MsgExec{}
+	if err := proto.Unmarshal(msg, m); err != nil {
+		return se, fmt.Errorf("Not a exec type: %w", err)
+	}
+
+	se = structs.SubsetEvent{
+		Type:   []string{"exec"},
+		Module: "authz",
+		Additional: map[string][]string{
+			"grantee": {m.Grantee},
+		},
+	}
+
+	for _, mg := range m.Msgs {
+		se.Additional["msgs"] = append(se.Additional["msgs"], string(mg.Value))
+	}
+
+	return se, nil
+}
+
+// AuthzGrantResponseToSub transforms authz.MsgGrantResponse sdk messages to SubsetEvent
+func AuthzGrantResponseToSub(msg []byte) (se structs.SubsetEvent, err error) {
+	m := &authz.MsgGrantResponse{}
+	if err := proto.Unmarshal(msg, m); err != nil {
+		return se, fmt.Errorf("Not a grant_response type: %w", err)
+	}
+
+	return structs.SubsetEvent{
+		Type:   []string{"grant_response"},
+		Module: "authz",
+	}, nil
+}
+
+// AuthzMsgRevokeToSub transforms authz.MsgRevoke sdk messages to SubsetEvent
+func AuthzMsgRevokeToSub(msg []byte) (se structs.SubsetEvent, err error) {
+	m := &authz.MsgRevoke{}
+	if err := proto.Unmarshal(msg, m); err != nil {
+		return se, fmt.Errorf("Not a revoke type: %w", err)
+	}
+
+	return structs.SubsetEvent{
+		Type:   []string{"revoke"},
+		Module: "authz",
+		Additional: map[string][]string{
+			"granter":      {m.Granter},
+			"grantee":      {m.Grantee},
+			"msg_type_url": {m.MsgTypeUrl},
+		},
+	}, nil
+}
+
+// AuthzMsgRevokeResponseToSub transforms authz.MsgRevokeResponse sdk messages to SubsetEvent
+func AuthzMsgRevokeResponseToSub(msg []byte) (se structs.SubsetEvent, err error) {
+	m := &authz.MsgRevokeResponse{}
+	if err := proto.Unmarshal(msg, m); err != nil {
+		return se, fmt.Errorf("Not a revoke_response type: %w", err)
+	}
+
+	return structs.SubsetEvent{
+		Type:   []string{"revoke_response"},
+		Module: "authz",
+	}, nil
+}
---
+++ figment/api/mapper/freegrant.go
@@ -0,0 +1,71 @@
+package mapper
+
+import (
+	"fmt"
+
+	"github.com/figment-networks/indexing-engine/structs"
+
+	"github.com/cosmos/cosmos-sdk/x/feegrant"
+	"github.com/gogo/protobuf/proto"
+)
+
+// FeegrantGrantAllowance transforms feegrant.MsgGrantAllowance sdk messages to SubsetEvent
+func FeegrantGrantAllowance(msg []byte) (se structs.SubsetEvent, err error) {
+	m := &feegrant.MsgGrantAllowance{}
+	if err := proto.Unmarshal(msg, m); err != nil {
+		return se, fmt.Errorf("Not a grant_allowance type: %w", err)
+	}
+
+	return structs.SubsetEvent{
+		Type:   []string{"grant_allowance"},
+		Module: "feegrant",
+		Additional: map[string][]string{
+			"granter":   {m.Granter},
+			"grantee":   {m.Grantee},
+			"allowance": {string(m.Allowance.Value)},
+		},
+	}, nil
+}
+
+// FeegrantGrantAllowanceResponse transforms feegrant.MsgGrantAllowanceResponse sdk messages to SubsetEvent
+func FeegrantGrantAllowanceResponse(msg []byte) (se structs.SubsetEvent, err error) {
+	m := &feegrant.MsgGrantAllowanceResponse{}
+	if err := proto.Unmarshal(msg, m); err != nil {
+		return se, fmt.Errorf("Not a grant_allowance_response type: %w", err)
+	}
+
+	return structs.SubsetEvent{
+		Type:   []string{"grant_allowance_response"},
+		Module: "feegrant",
+	}, nil
+}
+
+// FeegrantRevokeAllowance transforms feegrant.MsgRevokeAllowance sdk messages to SubsetEvent
+func FeegrantRevokeAllowance(msg []byte) (se structs.SubsetEvent, err error) {
+	m := &feegrant.MsgRevokeAllowance{}
+	if err := proto.Unmarshal(msg, m); err != nil {
+		return se, fmt.Errorf("Not a revoke_allowance type: %w", err)
+	}
+
+	return structs.SubsetEvent{
+		Type:   []string{"revoke_allowance"},
+		Module: "feegrant",
+		Additional: map[string][]string{
+			"granter": {m.Granter},
+			"grantee": {m.Grantee},
+		},
+	}, nil
+}
+
+// FeegrantRevokeAllowanceResponse transforms feegrant.MsgRevokeAllowanceResponse sdk messages to SubsetEvent
+func FeegrantRevokeAllowanceResponse(msg []byte) (se structs.SubsetEvent, err error) {
+	m := &feegrant.MsgRevokeAllowanceResponse{}
+	if err := proto.Unmarshal(msg, m); err != nil {
+		return se, fmt.Errorf("Not a revoke_allowance_response type: %w", err)
+	}
+
+	return structs.SubsetEvent{
+		Type:   []string{"revoke_allowance_response"},
+		Module: "feegrant",
+	}, nil
+}
